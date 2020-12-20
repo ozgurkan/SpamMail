d65fe466b73a79ab1b54a7cbd780f00cd5b4988a
özgür kan <galatasaray_ozgur_94@hotmail.com>
2020-12-20 12:17:07 +0300

    Proje dosyası ekle.

---
+++ SpamMail/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace SpamMail
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstMsg = new System.Windows.Forms.ListView();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstMsg
+            // 
+            this.lstMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstMsg.HideSelection = false;
+            this.lstMsg.Location = new System.Drawing.Point(12, 87);
+            this.lstMsg.Name = "lstMsg";
+            this.lstMsg.Size = new System.Drawing.Size(1581, 504);
+            this.lstMsg.TabIndex = 1;
+            this.lstMsg.UseCompatibleStateImageBehavior = false;
+            this.lstMsg.Visible = false;
+            this.lstMsg.SelectedIndexChanged += new System.EventHandler(this.lstMsg_SelectedIndexChanged);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblMsg.Location = new System.Drawing.Point(681, 30);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(70, 25);
+            this.lblMsg.TabIndex = 3;
+            this.lblMsg.Text = "label1";
+            this.lblMsg.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(151, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 30);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(151, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(383, 30);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Mail Adresi :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Şifre :";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(566, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 48);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Giriş";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1605, 603);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.lstMsg);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Spam Mail Detector";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListView lstMsg;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SpamMail/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Exchange.WebServices.Data;
+using Mono.Web;
+using JR.Utils.GUI.Forms;
+using SpamMailML.Model;
+using System.Text.RegularExpressions;
+using NPoco.FluentMappings;
+using Chilkat;
+
+namespace SpamMail
+{
+    public partial class Form1 : Form
+    {
+        ExchangeService exchange = null;
+        string[] basliklar = new string[500];
+        string[] icerikler = new string[500];
+        int i = 0;
+        string username;
+        string domain;
+        public Form1()
+        {
+            InitializeComponent();
+            lstMsg.Clear();
+            lstMsg.View = View.Details;
+            lstMsg.Columns.Add("Tarih/Saat", 170);
+            lstMsg.Columns.Add("Gönderen", 250);
+            lstMsg.Columns.Add("Konu", 380);
+            lstMsg.Columns.Add("İçerik", 600);
+            lstMsg.Columns.Add("Spam", 60);
+            lstMsg.Columns.Add("Rate", 80);
+            lstMsg.FullRowSelect = true;
+            lstMsg.ShowItemToolTips = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ConnectToExchangeServer()
+        {
+            lblMsg.Text = "Exchange Server'a bağlanılıyor....";
+            lblMsg.Refresh();
+            try
+            {
+                exchange = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+                exchange.Credentials = new WebCredentials(textBox1.Text, textBox2.Text, domain);
+                exchange.AutodiscoverUrl(textBox1.Text);
+
+                lblMsg.Text = "Exchange Server'a bağlandı : " + exchange.Url.Host+"\n Günlük Mailler Gösteriliyor.";
+                lblMsg.Refresh();
+
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Exchange Server'a bağlanırken hata oluştu.Lütfen maili ve şifreyi kontrol edin.\n" + ex.Message;
+                lblMsg.Refresh();
+            }
+
+        }
+        private void lstMsg_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListView.SelectedIndexCollection indices = lstMsg.SelectedIndices;
+            if (indices.Count > 0)
+            {
+                FlexibleMessageBox.Show(icerikler[indices[0]], basliklar[indices[0]]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            if (textBox1.Text=="" || textBox2.Text=="")
+            {
+                MessageBox.Show("Lütfen mail adresi ve şifrenizi giriniz.");
+            }
+            else
+            {
+                if (Regex.IsMatch(textBox1.Text, @"(@)"))
+                {
+                    username = textBox1.Text.Split('@')[0];
+                    domain = textBox1.Text.Split('@')[1];
+
+                    lblMsg.Visible = true;
+                    i = 0;
+                    lstMsg.Items.Clear();
+                    ConnectToExchangeServer();
+                    TimeSpan ts = new TimeSpan(0, -24, 0, 0);
+                    DateTime date = DateTime.Now.Add(ts);
+                    SearchFilter.IsGreaterThanOrEqualTo filter = new SearchFilter.IsGreaterThanOrEqualTo(ItemSchema.DateTimeReceived, date);
+
+                    if (exchange != null)
+                    {
+                        PropertySet itempropertyset = new PropertySet(BasePropertySet.FirstClassProperties);
+                        itempropertyset.RequestedBodyType = BodyType.Text;
+                        ItemView itemview = new ItemView(1000);
+                        itemview.PropertySet = itempropertyset;
+
+                        //FindItemsResults<Item> findResults = service.FindItems(WellKnownFolderName.Inbox, "subject:TODO", itemview);
+
+                        try
+                        {
+                            FindItemsResults<Item> findResults = exchange.FindItems(WellKnownFolderName.Inbox, filter, new ItemView(100));
+                            foreach (Item item in findResults)
+                            {
+                                lstMsg.Visible = true;
+                                item.Load(itempropertyset);
+                                String content = item.Body;
+                                icerikler[i] = content;
+
+                                ModelInput sampleData = new ModelInput()
+                                {
+                                    Col1 = content,
+                                };
+
+                                // Make a single prediction on the sample data and print results
+                                var predictionResult = ConsumeModel.Predict(sampleData);
+
+                                String durum;
+                                if (predictionResult.Prediction == "spam")
+                                {
+                                    durum = "YES";                                                                     
+                                }
+                                else
+                                {
+                                    durum = "NO";                                    
+                                }
+
+
+
+                                EmailMessage message = EmailMessage.Bind(exchange, item.Id);
+                                basliklar[i] = message.Subject;
+                                i++;
+                                ListViewItem listitem = new ListViewItem(new[]
+                                {
+                                message.DateTimeReceived.ToString(), message.From.Name.ToString() + "(" + message.From.Address.ToString() + ")", message.Subject,
+                                content,durum,predictionResult.Score.Max().ToString()                                
+                                });
+                                
+                                lstMsg.Items.Add(listitem);
+                                                              
+                            }
+                            if (findResults.Items.Count <= 0)
+                            {
+                                lstMsg.Items.Add("No Messages found!!");
+
+                            }
+                            colorListcolor(lstMsg);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Mail adresi veya şifre yanlış.");
+                            textBox1.Text = "";
+                            textBox2.Text = "";
+                            lblMsg.Text = "";
+                            lblMsg.Visible = false;
+                        }                    
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen doğru bir mail formatı giriniz.");
+                }              
+            }           
+        }
+
+        public static void colorListcolor(ListView lsvMain)
+        {
+
+            foreach (ListViewItem lvw in lsvMain.Items)
+            {
+                lvw.UseItemStyleForSubItems = false;
+
+                for (int i = 0; i < lsvMain.Columns.Count; i++)
+                {
+                    if (lvw.SubItems[4].Text.ToString() == "YES")
+                    {
+                        lvw.SubItems[4].BackColor = Color.Red;
+                        lvw.SubItems[4].ForeColor = Color.White;
+                    }
+                    else
+                    {
+                        lvw.SubItems[4].BackColor = Color.Green;
+                        lvw.SubItems[4].ForeColor = Color.White;
+                    }
+                }
+            }
+        }
+
+    }
+}
